76221ba6889bc63eb97106628f3e0e6e834faa81
Liquidnitro Games <arun@liquidnitrogames.com>
2026-07-04 11:13:48 +0530

    Access-model UX: tier gates + demo controls, persona switcher, Live Companion Mode (draft/Quick Sync, section leases), The Locker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cfb-super-app-v1';
+const CACHE = 'cfb-super-app-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
